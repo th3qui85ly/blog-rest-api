ba870b364041b4eb9eacc7ea393ddc17310794f9
= <=>
2022-05-30 02:42:06 +0530

    likeblog & listblog updated

--- Controllers/Blog.controller.js
+++ Controllers/Blog.controller.js
@@ -1,5 +1,6 @@
 const BlogModel = require("../Models/Blog.model");
 
+// creating a new blog
 const createData = async (req, res) => {
     try {
         const data = await BlogModel.create(req.body);
@@ -16,6 +17,45 @@ const createData = async (req, res) => {
     }
 }
 
+// listing all blog according to popularity
+const listBlog = async (req, res) => {
+    try {
+        const data = await BlogModel.find().sort({ likes: -1 });
+        res.status(200).json({
+            status: "success",
+            data
+        });
+    }
+    catch(err) {
+        res.status(401).json({
+            status: "failed",
+            err
+        });
+    }
+}
+
+// liking a blog
+const likeBlog = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const payload = await BlogModel.findById(id);
+        payload.likes = payload.likes + 1;
+        await BlogModel.findByIdAndUpdate(id, payload);
+        await res.status(200).json({
+            status: "success",
+            message: "Like updated!",
+            payload
+        });
+    }
+    catch(err) {
+        res.status(401).json({
+            status: "Like not sent"
+        });
+    }
+}
+
 module.exports = {
-    createData
+    createData,
+    listBlog,
+    likeBlog
 };
--- Routes/Blog.route.js
+++ Routes/Blog.route.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 
-const { createData } = require("../Controllers/Blog.controller");
+const { 
+    createData,
+    likeBlog,
+    listBlog
+} = require("../Controllers/Blog.controller");
 
 app.post("/createblog", createData);
+app.get("/listblog", listBlog);
+app.post("/likeblog/:id", likeBlog);
 
 module.exports = app;
