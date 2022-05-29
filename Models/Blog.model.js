8545ef6fb78ecc88958d9c34da9119bc8d8934ad
= <=>
2022-05-29 19:53:37 +0530

    createBlog fixed

--- Models/Blog.model.js
+++ Models/Blog.model.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
-const BlogSchema = new mongoose.schema({
+const BlogSchema = new mongoose.Schema({
     title: { required : true, type: String },
     description: { required: true, type: String },
     label: { type: String },
     likes: { type: Number, default: 0 },
     author: { required: true, type: String },
-    published: { type:String, default:false },
-    timestamp: { type:Date } 
+    published: { type: String, default: false },
+    timestamp: { type: Date, default: Date.now } 
 });
 
 module.exports = mongoose.model("blog", BlogSchema);
--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require('dotenv').config()
-const db_uri = process.env.DB_URI;
 
+const db_uri = process.env.DB_URI;
 const PORT = process.env.PORT || 1912;
+const routes = require("./Routes/Blog.route");
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
-
+app.use("/", routes);
 // Db Connect
 mongoose.connect(db_uri, () => {
     console.log(`Database connected through MongoDb`)
